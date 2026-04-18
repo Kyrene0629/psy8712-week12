618fdd7965bd2207c22695edc41efab4c8030ecc
Kyrene Li <li003631@umn.edu>
2026-04-17 21:03:23 -0500

    create a new lemmatized pre-processed corpus and write and check compare_them()

--- R/week12.R
+++ R/week12.R
@@ -19,7 +19,7 @@ library(jsonlite)
 #   select(upvotes = ups, title)
 
 # save the downloaded data files to data subfolder
-# saveRDS(reddit_posts, "../data/reddit_posts.csv")
+# saveRDS(reddit_posts, "../data/reddit_posts.rds")
 # write_csv(week12_tbl, "../data/week12_tbl.csv")
 
 # add code to import those files instead of downloading again
@@ -27,7 +27,48 @@ reddit_posts <- read_csv("../data/reddit_posts.csv")
 week12_tbl <- read_csv("../data/week12_tbl.csv")
 
 # Create corpus
-corpus <- VCorpus(VectorSource(blogs$documents))
+io_corpus_original <- VCorpus(VectorSource(week12_tbl$title))
+
+# Preprocessing using tm and qdap
+remove_io_terms <- content_transformer(function(x) {
+  x %>%
+    str_replace_all("\\bi\\s*/\\s*o\\s+psychology\\b", " ") %>%
+    str_replace_all("\\bi\\s*/\\s*o\\s+psych\\b", " ") %>%
+    str_replace_all("\\bi\\s*-\\s*o\\s+psychology\\b", " ") %>%
+    str_replace_all("\\bi\\s*-\\s*o\\s+psych\\b", " ") %>%
+    str_replace_all("\\bio\\s+psychology\\b", " ") %>%
+    str_replace_all("\\bio\\s+psych\\b", " ") %>%
+    str_replace_all("\\biopsychology\\b", " ") %>%
+    str_replace_all("\\bindustrial\\s+and\\s+organizational\\s+psychology\\b", " ") %>%
+    str_replace_all("\\bindustrial\\s+organizational\\s+psychology\\b", " ") %>%
+    str_replace_all("\\bindustrial[- ]organizational\\s+psychology\\b", " ") %>%
+    str_replace_all("\\bi\\s*/\\s*o\\b", " ") %>%
+    str_replace_all("\\bi\\s*-\\s*o\\b", " ") %>%
+    str_replace_all("\\bio\\b", " ")
+})
+
+io_corpus <- io_corpus_original %>%
+  tm_map(content_transformer(replace_abbreviation)) %>%
+  tm_map(content_transformer(replace_contraction)) %>%
+  tm_map(content_transformer(str_to_lower)) %>%
+  tm_map(remove_io_terms) %>%
+  tm_map(removeNumbers) %>%
+  tm_map(removePunctuation) %>%
+  tm_map(content_transformer(lemmatize_strings)) %>%
+  tm_map(removeWords, stopwords("en")) %>%
+  tm_map(stripWhitespace)
+
+# Function to compare the same randomly selected row from two corpora
+compare_them <- function(corpus_1, corpus_2) {
+  row_id <- sample(seq_along(corpus_1), 1)
+  
+  cat("Row selected:", row_id, "\n\n")
+  cat("Original:\n")
+  cat(as.character(corpus_1[[row_id]]), "\n\n")
+  cat("Processed:\n")
+  cat(as.character(corpus_2[[row_id]]), "\n")
+}
+
 
 # Visualization
 
